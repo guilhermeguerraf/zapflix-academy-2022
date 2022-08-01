fed1d12cf8fe55585b27057f0f7117b1293eaa27
Guilherme <guigfaria01@gmail.com>
2022-08-01 03:19:05 -0300

    Criando Generos, Artistas e Diretores

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :movies
-  resources :movie_genres
-  resources :directors
-  resources :artists
+  resources :movies, only: [:index, :new, :edit, :destroy]
+  resources :movie_genres, only: [:index, :new, :edit, :destroy]
+  resources :directors, only: [:index, :new, :edit, :destroy]
+  resources :artists, only: [:index, :new, :edit, :destroy]
   resource :home, only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
